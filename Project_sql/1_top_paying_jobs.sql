9b4c8a6da5f62bf520b5fa45a8abf654f77fb888
Kunal SIngh <kunalsngh2607@gmail.com>
2025-12-06 17:28:35 +0530

    Add Project_sql and update ignore rules

---
+++ Project_sql/1_top_paying_jobs.sql
@@ -0,0 +1,16 @@
+SELECT
+    job_id,
+    job_title,
+    salary_year_avg,
+    name AS comapany_name
+FROM 
+    job_postings_fact 
+LEFT JOIN 
+    company_dim ON job_postings_fact.company_id = company_dim.company_id
+WHERE 
+    job_title_short = 'Data Analyst' AND salary_year_avg is not null AND
+    job_location = 'Anywhere' AND
+    job_schedule_type = 'Full-time' 
+ORDER BY
+    salary_year_avg DESC
+limit 10
